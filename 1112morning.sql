8b4d39d5a87ab45db8c1dba91e42af5051495f57
dova12770 <aaaa2460@naver.com>
2024-11-12 10:06:25 +0900

    모닝퀴즈

---
+++ 1112morning.sql
@@ -0,0 +1,36 @@
+
+
+CREATE TABLE T_MENU_12
+(
+    menu_id NUMBER(5) PRIMARY KEY,
+    menu_name VARCHAR2(128) NOT NULL,
+    price NUMBER(10),
+    item VARCHAR2(64),
+    menu_date date
+);
+
+INSERT INTO T_MENU_12 
+VALUES (1,'싸이버거',6000,'완전식품',SYSDATE);
+
+INSERT INTO T_MENU_12 
+VALUES (2,'콩나물비빔밥',7000,'탄수화물',SYSDATE);
+
+INSERT INTO T_MENU_12 
+VALUES (3,'닭가슴살',3000,'단백질',SYSDATE);
+
+INSERT INTO T_MENU_12 
+VALUES (4,'베트남쌀국수',8000,'탄수화물',SYSDATE);
+
+INSERT INTO T_MENU_12 
+VALUES (5,'라면',2000,'밀가루',SYSDATE);
+
+select * from T_MENU_12;
+
+UPDATE T_MENU_12
+SET price=8500
+WHERE menu_id =4;
+
+SELECT *
+--DELETE  
+FROM T_MENU_12
+WHERE menu_id = 5;
--- 퀴즈연습.sql
+++ 퀴즈연습.sql
@@ -27,10 +27,51 @@ from dept2;
 
 
 
-select area
-from dept2
-where
-    dcode=( select deptno
+select e.empno, e.name, e.deptno, d.dname,d.area,e.pay
+from emp2 e, dept2 d
+where 
+    e.deptno = d.dcode
+    and
+    pay > (select avg(pay)
             from emp2
-            where 
-                name = 'AL Pacino');
+            where
+                deptno in(select dcode
+                            from dept2 
+                            where
+                            area = (select area
+                                    from dept2
+                                    where
+                                        dcode=( select deptno
+                                                from emp2
+                                                where 
+                                                    name = 'AL Pacino'))));
+
+select *
+from panmae;
+
+select *
+from product;
+
+select *
+from gift;
+
+
+
+select 
+    p.p_date 판매일자,
+    p.p_code 상품코드,
+    pr.p_name 상품이름,
+    pr.p_price 상품가,
+    p.p_qty 구매수량,
+    p.p_total 총금액,
+    p.p_total*100 적립포인트,
+    decode(TO_char(substr(p.p_date,5,4)),'0101',p.p_total*200,p.p_total*100) 새해2배적립포인트,
+    g.gname 사은품명,
+    g.g_start 포인트START,
+    g.g_end 포인트END
+from panmae p, product pr, gift g
+where 
+    p.p_code = pr.p_code
+    and
+    (decode(TO_char(substr(p.p_date,5,4)),'0101',p.p_total*200,p.p_total*100)) between g.g_start and g.g_end
+order by p.p_date;
